92e4996e25e7f96df3754063e91d22f9dec76a66
AndrzejPeszel <andrzej.peszel@icloud.com>
2025-11-03 12:08:53 +0100

    fixed solution

--- app/cafe.py
+++ app/cafe.py
@@ -13,15 +13,21 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             name = visitor.get("name", "Unknown visitor")
-            raise NotVaccinatedError(f"{name} is not vaccinated.")
+            raise NotVaccinatedError(
+                f"{name} is not vaccinated."
+            )
 
         expiration_date = visitor["vaccine"].get("expiration_date")
         if not expiration_date or expiration_date < datetime.date.today():
             name = visitor.get("name", "Unknown visitor")
-            raise OutdatedVaccineError(f"{name}'s vaccine is expired or missing.")
+            raise OutdatedVaccineError(
+                f"{name}'s vaccine is expired or missing."
+            )
 
         if not visitor.get("wearing_a_mask", False):
             name = visitor.get("name", "Unknown visitor")
-            raise NotWearingMaskError(f"{name} is not wearing a mask.")
+            raise NotWearingMaskError(
+                f"{name} is not wearing a mask."
+            )
 
         return f"Welcome to {self.name}"
